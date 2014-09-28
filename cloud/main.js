9f27500a75c559f661c6c4551ce193c33ca94be0
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2014-09-28 02:04:44 -0500

    cloud function to schedule deal push

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,42 @@ require('cloud/app.js');
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+var scarce = true;
+Parse.Cloud.afterSave("Deal", function(request, response){
+			channel = []
+			channel.push(request.object.get("community_name"));
+			Parse.Push.send({
+			  push_time: request.object.get("deal_start_date"),
+			  channels: channel,
+			  data: {
+			    alert: request.object.get("name") + " at " + request.object.get("location_name"),
+			    title: "BarLift New Deal"
+			  }, 
+			  success: function() {
+				console.log(JSON.stringify(object));
+			  },
+			  error: function(error) {
+				console.log("Error" + JSON.stringify(object));
+			  }
+				
+			});
+			if(request.object.get("deal_qty") < 10 && scarce){
+				Parse.Push.send({
+				  channels: channel,
+				  data: {
+				    alert: "Only " + request.object.get("deal_qty") + " deals left! " + "\n"+request.object.get("deal_name") + " at " + request.object.get("location_name"),
+				    title: "Hurry Only A Few Deals Left"
+				  }, 
+				  success: function() {
+				    scarce = false; //prevent many push from being sent
+				    console.log("Success" + JSON.stringify(object));
+
+				  },
+				  error: function(error) {
+				console.log("Error Push" + JSON.stringify(object));
+				  }
+					
+				});
+			}
+});
