c74317ac9c5a54b98e1074b800ce8ebe471bba3f
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2015-05-17 18:07:40 -0500

    nu com

--- cloud/main.js
+++ cloud/main.js
@@ -98,6 +98,7 @@ Parse.Cloud.afterSave(Parse.User, function(request) {
             }
         });
     } 
+    request.object.set('community_name', 'Northwestern');
     if(request.object.get('university_name') == "Kellogg" || request.object.get('university_name') == "Northwestern"){
     	request.object.set('community_name', 'Northwestern');
     	request.object.save();
